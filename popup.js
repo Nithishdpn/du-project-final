be25aea2eb7898d7e192798d8486581755f011bd
Keerthudarshu <Keerthudarshu06@gmail.com>
2025-08-28 12:39:45 +0530

    google sheet notification updated

--- popup.js
+++ popup.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
     // Google Apps Script URL for form submission
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxTpCjzJuYLCGcGhgvzZ5o8ZrpnQhHXvGT5lgjfdMN4Lkz4RaNdiqxqRgHaXfsvYvv89A/exec'; // Replace with your Apps Script Web App URL
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzgbP4juabn6BV1-7Xpu_OKsMDdGFjYPoJwoOH_f86ZDnP3L87hplreZPDm6vpcUIr1DA/exec'; // Replace with your Apps Script Web App URL
 
     document.getElementById('popupContactForm').addEventListener('submit', function(e) {
       e.preventDefault();
